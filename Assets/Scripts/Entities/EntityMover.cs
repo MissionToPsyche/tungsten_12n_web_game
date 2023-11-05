412be0b5b4ca22078d1b8feb57c0e04655aba726
Abhisekhar Gandavarapu <34994192+Abhig2002@users.noreply.github.com>
2023-11-05 01:58:40 -0700

    Added entity creation in new game layout

--- Assets/Scripts/Entities/EntityMover.cs
+++ Assets/Scripts/Entities/EntityMover.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class EntityMover : MonoBehaviour
 {
-    Vector2 difference = Vector2.zero;
+    public float moveSpeed = 5.0f;
+    public float rotateSpeed = 90.0f; // Rotation speed in degrees per second
 
-    private void OnMouseDown()
+    private bool isMoving = false;
+
+    private Rigidbody2D rb;
+
+    void Start()
     {
-        difference = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    private void OnMouseDrag()
+
+    void Update()
     {
-        transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - difference;
+        if (isMoving)
+        {
+            float horizontalInput = 0.0f;
+            float verticalInput = 0.0f;
+
+            // Check for IJKL key input (movement)
+            if (Input.GetKey("i"))
+                verticalInput = 1.0f;
+            if (Input.GetKey("k"))
+                verticalInput = -1.0f;
+            if (Input.GetKey("j"))
+                horizontalInput = -1.0f;
+            if (Input.GetKey("l"))
+                horizontalInput = 1.0f;
+
+            // Check for U and O key input (rotation)
+            if (Input.GetKey("u"))
+                RotateEntity(-1.0f); // Rotate left
+            if (Input.GetKey("o"))
+                RotateEntity(1.0f); // Rotate right
+
+            // Calculate the movement direction
+            Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0.0f);
+
+            // Normalize the direction to prevent faster diagonal movement
+            if (moveDirection.magnitude > 1.0f)
+            {
+                moveDirection.Normalize();
+            }
+
+            // Move the entity
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void RotateEntity(float direction)
+    {
+        // Rotate the entity based on the provided direction
+        transform.Rotate(Vector3.forward, direction * rotateSpeed * Time.deltaTime);
+    }
+
+    public void SetIsMoving(bool value)
+    {
+        isMoving = value;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Asteroid"))
+        {
+            // Set the entity's velocity to zero to make it stop upon collision
+            rb.velocity = Vector2.zero;
+        }
     }
 }
