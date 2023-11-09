6c579c49c2c8976c816ee42191ab08009cf3342e
jdun24 <34287430+jdun24@users.noreply.github.com>
2023-11-09 13:36:10 -0700

    expanded world
    Removed some SpaceShip logic.

--- Assets/Scripts/Asteroid/Asteroid.cs
+++ Assets/Scripts/Asteroid/Asteroid.cs
@@ -1,7 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
+
+public enum AsteroidClass
+{
+    S_Class, A_Class, B_Class, C_Class, D_Class, F_Class
+}
 
 public class Asteroid : MonoBehaviour
 {
+    public float size { get; set; }
+    private GameObject prefab;
+    private List<Resource> resourceList = new List<Resource>();
+    public SpawnResources sr;
+    public AsteroidClass asteroidClass { get; set; }
+
+    public void InstantiateAsteroid(float Size, int numberOfResources, AsteroidClass asteroidClass, GameObject prefab)
+    {
+        // Set the size based on the average scale of the GameObject
+        size = Size;
+        this.asteroidClass = asteroidClass;
+        this.prefab = prefab;
+        if (prefab != null)
+        {
+            if (asteroidClass != AsteroidClass.F_Class)
+            {
+                sr = new SpawnResources(Size, numberOfResources, this);
+            }
+        }
+        else
+        {
+            Debug.LogError("Prefab not assigned to Asteroid. Please assign the prefab to asteroid Manager in the Unity Editor.");
+        }
+    }
+
+    public void SpawnResource(Resource addedResource, int iter)
+    {
+        GameObject newResourceObject = Instantiate(prefab, addedResource.Position + (Vector2)transform.position, Quaternion.identity);
+        setNewResourceObject(newResourceObject, addedResource, iter);
+        
+    }
+
+    //Makes the resource the desired color
+    public void setNewResourceObject(GameObject newResourceObject, Resource addedResource, int iter)
+    {
+        SpriteRenderer spriteRenderer = newResourceObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = addedResource.Color;
+        spriteRenderer.sortingLayerName = "Foreground";
+
+        //Debug.Log(this.name + " " + transform.parent.name);
+        newResourceObject.transform.localScale = addedResource.depositSize;
+        newResourceObject.transform.parent = this.transform;
+        newResourceObject.name = $"{addedResource.Name}_" + iter;
+        resourceList.Add(addedResource);
+    }
+
+    //Funciton purely used by SpawnResources.cs --: spawnObjects() :-- here so we only ever need one resourceList per asteroid
+    public bool IsValidPosition(Vector3 position, int i)
+    {
+        float minDistanceBetweenObjects = 7f;
+        foreach (Resource resource in resourceList)
+        {
+            if (resource != null)
+            {
+                float distanceToResource = Vector2.Distance(resource.Position, position);
+                //keeps resources from being too close to eachother, as well as outside of the asteroid in edge cases
+                if (distanceToResource < minDistanceBetweenObjects)
+                {
+                    return false; // Position is too close to an existing resource
+                }
+                //Debug.Log($"Resource_{i} distance to {resource.name}: {distanceToResource}");
+            }
+        }
+
+        return true;
+    }
 }
--- Assets/Scripts/Asteroid/ResourceGen/Resource.cs
+++ Assets/Scripts/Asteroid/ResourceGen/Resource.cs
@@ -3,58 +3,148 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+public enum Rarity
+{
+    Common, Commodity, Exotic
+}
+
 public class Resource
 {
-    public Vector2 Position { get; set; }
+    //Resource info
     public string Name { get; set; }
+    public Vector2 Position { get; set; }
     public Color Color { get; set; }
-    public int depositAmount;
-    public float depositSize;
-    public GameObject prefab; //for when each resource has its own prefab
+    public Rarity rarity;
+
+    //Deposit info
+    public Vector3 depositSize { get; set; }
+    public int depositAmount { get; set; }
     public bool isDepleted;
 
-    // Constructor to initialize properties
+    //public GameObject prefab; //for when each resource has its own prefab
+
     public Resource(Vector2 pos)
     {
         Position = pos;
         depositSize = randomizeSize();
         //amount of resources in an ore deposit is related to its size
-        depositAmount = (int)(depositSize * 100);
+        depositAmount = (int)(depositSize.x + depositSize.y) * 25;
     }
 
-    protected float randomizeSize()
+    protected Vector3 randomizeSize()
     {
-        return Random.Range(0.5f, 3.5f);
+        float x = Random.Range(0.5f, 5f);
+        float y = Random.Range(0.5f, 5f);
+
+        Vector3 valToReturn = new Vector3(x, y, y);
+        return valToReturn;
     }
 
 }
 
-
+//----------------------<COMMON RESOURCES>----------------------//
 public class Iron : Resource
 {
-
     public Iron(Vector2 pos) : base(pos)
     {
         Name = "Iron";
         Color = Color.red;
         Position = pos;
         depositSize = base.randomizeSize();
-        depositAmount = (int)(depositSize * 100);
+        depositAmount = (int)(depositSize.x + depositSize.y) * 100;
+        rarity = Rarity.Common;
     }
-
 }
 
 public class Nickel : Resource
 {
-    public int NickelValue { get; set; }
-
     public Nickel(Vector2 pos) : base(pos)
     {
         Name = "Nickel";
         Color = Color.gray;
         Position = pos;
         depositSize = base.randomizeSize();
-        depositAmount = (int)(depositSize * 100);
+        depositAmount = (int)(depositSize.x + depositSize.y) * 100;
+        rarity = Rarity.Common;
     }
+}
 
+public class Silver : Resource
+{
+    public Silver(Vector2 pos) : base(pos)
+    {
+        Name = "Silver";
+        Color = Color.white;
+        Position = pos;
+        depositSize = base.randomizeSize();
+        depositAmount = (int)(depositSize.x + depositSize.y) * 100;
+        rarity = Rarity.Common;
+    }
+}
+
+//----------------------<COMMODITY RESOURCES>----------------------//
+public class Platinum : Resource
+{
+    public Platinum(Vector2 pos) : base(pos)
+    {
+        Name = "Platinum";
+        Color = Color.cyan;
+        Position = pos;
+        depositSize = base.randomizeSize();
+        depositAmount = (int)(depositSize.x + depositSize.y) * 50;
+        rarity = Rarity.Commodity;
+    }
+}
+
+public class Gold : Resource
+{
+    public Gold(Vector2 pos) : base(pos)
+    {
+        Name = "Gold";
+        Color = Color.yellow;
+        Position = pos;
+        depositSize = base.randomizeSize();
+        depositAmount = (int)(depositSize.x + depositSize.y) * 50;
+        rarity = Rarity.Commodity;
+    }
+}
+
+public class Technetium : Resource
+{
+    public Technetium(Vector2 pos) : base(pos)
+    {
+        Name = "Technetium";
+        Color = Color.green;
+        Position = pos;
+        depositSize = base.randomizeSize();
+        depositAmount = (int)(depositSize.x + depositSize.y) * 50;
+        rarity = Rarity.Commodity;
+    }
+}
+
+//----------------------<EXOTIC RESOURCES>----------------------//
+public class Tungsten : Resource
+{
+    public Tungsten(Vector2 pos) : base(pos)
+    {
+        Name = "Tungsten";
+        Color = Color.blue;
+        Position = pos;
+        depositSize = base.randomizeSize();
+        depositAmount = (int)(depositSize.x + depositSize.y) * 25;
+        rarity = Rarity.Exotic;
+    }
+}
+
+public class Iridium : Resource
+{
+    public Iridium(Vector2 pos) : base(pos)
+    {
+        Name = "Iridium";
+        Color = Color.black;
+        Position = pos;
+        depositSize = base.randomizeSize();
+        depositAmount = (int)(depositSize.x + depositSize.y) * 25;
+        rarity = Rarity.Exotic;
+    }
 }
